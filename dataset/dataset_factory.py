a7655ee75b49f0d7b143a4993611484a0032f9b3
p-omahony <paul.omahony@outlook.fr>
2022-12-22 18:45:04 +0100

    add detec dataset

--- dataset/dataset_factory.py
+++ dataset/dataset_factory.py
@@ -1,6 +1,11 @@
 from .preprocessing import get_mapping, preprocess_img
 
 from torch.utils.data import Dataset
+import torch
+from skimage import io
+import numpy as np
+
+import os
 
 
 class IAMData(Dataset):
@@ -16,3 +21,39 @@ class IAMData(Dataset):
         image, word = preprocess_img(img_name, word)
 
         return image, word
+
+
+class FormDataset(Dataset):
+    def __init__(self, image_folder_path, labels, transform=None, target_transform=None):
+        self.image_folder_path = image_folder_path
+        self.labels = labels
+        self.transform = transform
+        self.target_transform = target_transform
+        
+    def __len__(self) : 
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_folder_path, self.labels.iloc[idx]['name'] + '.png')
+        image = io.imread(img_path, as_gray=True)
+        if self.transform:
+            img = self.transform(image)
+        
+        xmins = self.labels.iloc[idx]['xmin']
+        ymins = self.labels.iloc[idx]['ymin']
+        xmaxs = self.labels.iloc[idx]['xmax']
+        ymaxs = self.labels.iloc[idx]['ymax']
+        
+        
+        boxes = []
+        labels = []
+        for k in range(len(xmins)) : 
+            boxes.append([xmins[k], ymins[k],xmaxs[k],ymaxs[k]])
+        
+        boxes = torch.as_tensor(np.array(boxes))
+            
+        target = {}
+        target['boxes'] = boxes
+            
+        
+        return img, target
