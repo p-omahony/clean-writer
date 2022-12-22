76f4f21da4f10c4948af5ed24af7664a8f467ec9
p-omahony <paul.omahony@outlook.fr>
2022-12-22 13:07:36 +0100

    add dl data script

---
+++ scripts/install_data.sh
@@ -0,0 +1,5 @@
+cd ./data
+gdown https://drive.google.com/uc?id=1KN_0oJHjH7g1GtqgdpQpBKqrnDs8VDaI
+gdown https://drive.google.com/uc?id=1Y7H1TIS6OvjV9s8gBGStqLyQuhok1-NT
+tar xvzf ascii.tgz
+tar xvzf lines.tgz
